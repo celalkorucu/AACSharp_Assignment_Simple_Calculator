4bb69ab97718146d5061de1853942e8a78f7bb6b
celalkorucu <21010310079@ogrenci.bartin.edu.tr>
2025-03-17 09:27:54 +0300

    Add project files.

---
+++ AACSharp_Assignment_Simple_Calculator/Classes/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AACSharp_Assignment_Simple_Calculator.Classes
+{
+    public class Calculator { 
+
+
+        public static  double Calculate(double firstNumber , double secondNumber ,string process)
+        {
+            double result = 0;
+            switch (process)
+            {
+                case "+": Console.WriteLine("Toplama işlemi başlıyor . . . ");
+                    Console.Write(firstNumber + " " + process + " " + secondNumber + " = "); 
+                    result = firstNumber + secondNumber; break;
+                case "-":
+                    Console.WriteLine("Çıkarma işlemi başlıyor . . . ");
+                    Console.Write(firstNumber + " " + process + " " + secondNumber + " = ");
+                    result = firstNumber - secondNumber; break;
+                case "*":
+                    Console.WriteLine("Çarpma işlemi başlıyor . . . ");
+                    Console.Write(firstNumber + " " + process + " " + secondNumber + " = ");
+                    result = (firstNumber * secondNumber); break;
+                case "/":
+                    Console.WriteLine("Bölme işlemi başlıyor . . . ");
+                    Console.Write(firstNumber + " " + process + " " + secondNumber + " = ");
+                    result = ( firstNumber / secondNumber); break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AACSharp_Assignment_Simple_Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AACSharp_Assignment_Simple_Calculator.Classes;
+
+double result = Calculator.Calculate(2, 3, "+");
+Console.WriteLine(result);
+
+result = Calculator.Calculate(2, 5, "/");
+Console.WriteLine(result);
+
+result = Calculator.Calculate(6, 7, "*");
+Console.WriteLine(result);
+
+result = Calculator.Calculate(9, 12, "-");
+Console.WriteLine(result);
+
+
+
+
